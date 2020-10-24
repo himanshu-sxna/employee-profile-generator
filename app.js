1964fcc7edc60996f0122436e32a2e3c6f27e127
Himanshu Saxena <contact@hellohimanshu.com>
2020-10-24 21:53:21 +1030

    updated classes and tests

---
+++ app.js
@@ -0,0 +1,11 @@
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
